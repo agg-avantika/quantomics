47573ba56ad2bd1fcc73b28e04cecbdd423ebae2
agg-avantika <avantika.aggarwal@gmail.com>
2019-01-14 03:59:29 +0530

    base set up ready

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                React Initialised!
+            </div>
+        );
+    }
+}
+
+export default App;
